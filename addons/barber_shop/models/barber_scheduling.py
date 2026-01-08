6cfb8a40c20153b2bcdc5e11b1796aa0b65aa995
balb1not <balb1not@users.noreply.github.com>
2026-01-08 11:09:26 -0300

    VERFIICAR BARBER SCHEDULING FUNÇÃO CREATE E DEBUGAR SEQUENCIA

--- addons/barber_shop/models/barber_scheduling.py
+++ addons/barber_shop/models/barber_scheduling.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class BarberScheduling(models.Model):
     _name = 'barber.scheduling'
@@ -9,3 +9,17 @@ class BarberScheduling(models.Model):
     barber_id = fields.Many2one('barber.barber', string="Barber")
     service_ids = fields.Many2many('barber.services', string="Barber Services")
     start_time = fields.Float(string="Start Time")
+
+    total_amount = fields.Float(string='Total Amount', compute='_compute_total_amount', store=True)
+
+    @api.depends('service_ids.price')
+    def _compute_total_amount(self):
+        for rec in self:
+            rec.total_amount = sum(service.price for service in rec.service_ids)
+            
+    @api.model
+    def create(self, vals_list):
+        vals_list['name'] = self.env['ir.sequence'].next_by_code('seq.scheduling')
+        return super(BarberScheduling, self).create(vals_list)
+
+    
